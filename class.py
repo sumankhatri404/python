f93d339c446b6e7b7956891b18422d01814ac43a
suman khatri <sumitkhatri858@gmail.com>
2022-08-23 08:47:06 +0545

    exceptions,class,inheritance in python

--- class.py
+++ class.py
@@ -13,29 +13,29 @@
 '''creating method and inheritance '''
 
 
-class Animal:
-
-    def __init__(self, name,sing ):
-        self.name = name
-        self.sing=sing
-
-    def fly(self):
-        print("{} can fly".format(self.name))
-
-    def song(self):
-        print("{} can sing a {} song".format(self.name,self.sing))
-
-
-class penguin(Animal):
-
-    def __int__(self):
-        super().__init__()
-        print("penguin is ready")
-
-    def swim(self):
-        print("penguin can swim")
-
-
-obj1 = Animal("parrot","happy")
-obj1.fly()
-obj1.song()
+# class Animal:
+#
+#     def __init__(self, name,sing ):
+#         self.name = name
+#         self.sing=sing
+#
+#     def fly(self):
+#         print("{} can fly".format(self.name))
+#
+#     def song(self):
+#         print("{} can sing a {} song".format(self.name,self.sing))
+#
+#
+# class penguin(Animal):
+#
+#     def __int__(self):
+#         super().__init__()
+#         print("penguin is ready")
+#
+#     def swim(self):
+#         print("penguin can swim")
+#
+#
+# obj1 = Animal("parrot","happy")
+# obj1.fly()
+# obj1.song()
